7a991df359ab400d442b6c6b7c4d849b47322a43
binsee <binsee@163.com>
2018-05-28 18:23:46 +0800

    feat: 增加输入json文件、按字段过滤操作
    1. 支持使用指定json文件输入参数
    2. 支持仅操作指定字段
    3. 支持仅回显指定字段

--- cli.js
+++ cli.js
@@ -11,9 +11,13 @@ cli
   .description('Read and Write project info for e lang code file.')
   .usage('[options] <efile>')
   .version(Package.version)
+  .option('-i, --input-json [file]', 'json file to read')
   .option('-o, --out-json [file]', 'json file to write')
-  .option('-s, --save-to-file [file]', 'file to write to')
+  .option('-s, --save-to-file [file]', 'E lang file to write to')
   .option('-w, --write', 'save new value to e lang file')
+  .option('-f, --read-json-fields [field, ...]', 'read fields from input json file')
+  .option('-F, --write-json-fields [field, ...]', 'write fields to out json file')
+
   .option('-n, --name <value>', 'change project name')
   .option('-d, --description <value>', 'change project description')
   .option('-a, --author <value>', 'change project author')
@@ -21,8 +25,8 @@ cli
   .option('-m, --minor <n>', 'change minor version number')
   .option('-p, --patch <n>', 'change patch version number')
   .option('-d, --date <n>', 'change date version number')
-  .option('-v, --project-version <ver>', 'change project version number')
-  .option('-f, --project-full-version <ver>', 'change full version number')
+  .option('-v, --ver <ver>', 'change project version number')
+  .option('-f, --full-version <ver>', 'change full version number')
   .parse(process.argv)
 
   .on('--help', () => {
@@ -36,13 +40,13 @@ cli
   > output project info to json:
 
     $ ${cli._name} -o package.json file.e
-    $ ${cli._name} -o package.json file.ec
+    $ ${cli._name} -o package.json -F version,name,author file.ec
 
   > update project info:
 
-    $ ${cli._name} -w -n "test project" file.e
     $ ${cli._name} -w -n "test project" -v 1.1.0 file.e
     $ ${cli._name} -w -n "test project" -v 1.1.0 -s new-file.e file.e
+    $ ${cli._name} -w -i ./info.json -f version -s new-file.e file.e
     `
     console.log(example)
 
@@ -75,6 +79,9 @@ const fields =
     'minor',
     'patch',
     'date',
+    'ver',
+    'version',
+    'fullVersion',
   ]
 
 fields.forEach(key => {
@@ -83,12 +90,37 @@ fields.forEach(key => {
   }
 })
 
-if (cli.projectVersion) {
-  etool.user.data.version = cli.projectVersion
+
+if (cli.inputJson) {
+  let json       = readJson(cli.inputJson)
+  let readFields = fields
+  if (cli.readJsonFields) {
+    const fields     = (cli.readJsonFields + '').split(',')
+          readFields = fields.filter(key => fields.includes(key))
+  }
+  readFields.forEach(key => {
+    if (key === 'version') {
+      key = 'ver'
+    }
+    if (json.hasOwnProperty(key)) {
+      cli[key] = json[key]
+    }
+  })
 }
-if (cli.projectFullVersion) {
-  etool.user.data.fullVersion = cli.projectFullVersion
-}
+
+fields.forEach(key => {
+  if (cli[key] && typeof cli[key] !== 'function') {
+    if (key === 'ver') {
+      etool.user.data['version'] = cli['ver']
+    } else if (key === 'version') {
+      return
+    } else if (etool.user.data[key].value !== undefined) {
+      etool.user.data[key].value = cli[key]
+    } else if (etool.user.data.hasOwnProperty(key)) {
+      etool.user.data[key] = cli[key]
+    }
+  }
+})
 
 const info = {
   name       : etool.user.data.name.value,
@@ -98,19 +130,31 @@ const info = {
   homepage   : etool.user.data.homepage.value,
   version    : etool.user.data.version,
   fullVersion: etool.user.data.fullVersion,
-  ver_major  : etool.user.data.major.value,
-  ver_minor  : etool.user.data.minor.value,
-  ver_patch  : etool.user.data.patch.value,
-  ver_date   : etool.user.data.date.value,
+  major      : etool.user.data.major.value,
+  minor      : etool.user.data.minor.value,
+  patch      : etool.user.data.patch.value,
+  date       : etool.user.data.date.value,
 }
 
 if (cli.outJson) {
-  let json = readJson(cli.outJson) || {}
-      json = Object.assign({}, json, info)
+  let json        = readJson(cli.outJson)
+  let writeFields = fields
+  if (cli.writeJsonFields) {
+    const fields      = (cli.writeJsonFields + '').split(',')
+          writeFields = fields.filter(key => fields.includes(key))
+  }
+  writeFields.forEach(key => {
+    if (key === 'ver') {
+      key = 'version'
+    }
+    if (info.hasOwnProperty(key)) {
+      json[key] = info[key]
+    }
+  })
   try {
     fs.writeFileSync(cli.outJson, JSON.stringify(json, null, 2))
   } catch (e) {
-    console.log('Write file error: ', e.message)
+    console.log('Write to json file error: ', e.message)
   }
 }
 
@@ -120,11 +164,28 @@ if (cli.write) {
     const newBuf = etool.save()
     fs.writeFileSync(newFile, newBuf)
   } catch (e) {
-    console.log('Write to file error: ', e.message)
+    console.log('Write to E lang file error: ', e.message)
   }
 }
 
-console.log(info)
+
+let showFields = fields
+if (cli.writeJsonFields) {
+  const fields     = (cli.writeJsonFields + '').split(',')
+        showFields = fields.filter(key => fields.includes(key))
+}
+const json = {}
+showFields.map(key => {
+  if (key === 'ver') {
+    key = 'version'
+  }
+  if (info[key]) {
+    json[key] = info[key]
+  }
+})
+if (Object.keys(json).length > 0) {
+  console.log(json)
+}
 
 function readJson(file) {
   let json = {}
